39c480488dcbef3ea4b11321e24e89b1cba477d7
Petr Praus <petr@praus.net>
2011-05-15 21:36:00 +0200

    Teardown all interfaces but set up example on only a specific interfaces

--- examples/settings.py
+++ examples/settings.py
@@ -1,15 +1,3 @@
-def scan_interfaces():
-    """Parses a list of all interfaces reported by `ip link`"""
-    import subprocess
-    import re
-    ifcs = []
-    out = subprocess.check_output(["ip", "link"]).split('\n')
-    for line in out:
-        m = re.match("^[0-9]+:[ ]([a-z0-9]+):", line)
-        if m:
-            ifcs.append(m.group(1))
-    return ifcs
-
-EMU_INTERFACES = scan_interfaces()
+EMU_INTERFACES = ('lo',)
 EMU_NOSHAPE_PORTS = ('8000',)
 
