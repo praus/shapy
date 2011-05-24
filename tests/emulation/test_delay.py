38186627eb286a1c97480630137f9e9270978f9f
Petr Praus <petr@praus.net>
2011-05-24 13:50:51 +0200

    Extended delay unit test

--- tests/emulation/test_delay.py
+++ tests/emulation/test_delay.py
@@ -9,8 +9,8 @@ register_settings('tests.emulation.settings')
 
 class TestCWCDelay(unittest.TestCase, ShaperMixin):
     def setUp(self):
-        self.server_addr = ('127.0.0.2', 55000)
-        self.client_addr = ('127.0.0.3', 55001)
+        self.server_addr = ('127.0.0.2', 55000) # 5 ms delay
+        self.client_addr = ('127.0.0.3', 55001) # 30 ms delay
         ShaperMixin.setUp(self)
     
     def test_delay(self):
@@ -18,7 +18,13 @@ class TestCWCDelay(unittest.TestCase, ShaperMixin):
         s = self.server_addr[0]
         ping(c, s, 1) # ARP
         est_delay = self.estimate_delay(c, s)
-        self.assertAlmostEqual(ping(c, s), est_delay*2, delta=2)
+        
+        self.assertAlmostEqual(ping(c, s), est_delay*2, delta=1)
+        self.assertAlmostEqual(ping(s, c), est_delay*2, delta=1)
+        self.assertAlmostEqual(ping(c, "127.0.0.1"), 60, delta=0.5)
+        self.assertAlmostEqual(ping("127.0.0.1", c), 60, delta=0.5)
+        self.assertAlmostEqual(ping(s, "127.0.0.1"), 10, delta=1)
+        self.assertAlmostEqual(ping("127.0.0.1", s), 10, delta=1)
     
     def tearDown(self):
         ShaperMixin.tearDown(self)
